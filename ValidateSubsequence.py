7921c21b8dbf733b49a1f6b70e7366027acce74e
GeorgeCouch <george.couch.14@gmail.com>
2021-06-25 15:49:46 -0400

    completed using while loops

--- ValidateSubsequence.py
+++ ValidateSubsequence.py
@@ -1,10 +1,9 @@
-# array = [5,1,22,25,6,-1,8,10]
-# sequence = [1,6,-1.10]
+
 def isValidSubsequence(array, sequence):
     newSequence = []
     i = 0
-    j = 0
     while(i < len(array)):
+        j = 0
         while(j < len(sequence)):
             print("array[i] = " + str(array[i]))
             print("sequence[j] = " + str(sequence[j]))
@@ -15,19 +14,26 @@ def isValidSubsequence(array, sequence):
                     newSequence.pop(0)
                 print(newSequence)
                 i += 1
-                j = i
-        if(newSequence == sequence and len(newSequence) <= len(array)):
-            print("array = " + str(array) + " Elements = " + str(len(array)))
-            print("sequence = " + str(sequence) + " Elements = " + str(len(sequence)))
-            print("subsequence = " + str(newSequence) + " Elements = " + str(len(newSequence)))
-            return True
-        else:
-            print("array = " + str(array) + " Elements = " + str(len(array)))
-            print("sequence = " + str(sequence) + " Elements = " + str(len(sequence)))
-            print("subsequence = " + str(newSequence) + " Elements = " + str(len(newSequence)))
-            return False
+                j = 0
+                if(i == len(array)):
+                    break
+            j += 1
+        if(i == len(array)):
+            break
+        j = 0
+        i += 1
+    if(newSequence == sequence):
+        print("array = " + str(array) + " Elements = " + str(len(array)))
+        print("sequence = " + str(sequence) + " Elements = " + str(len(sequence)))
+        print("subsequence = " + str(newSequence) + " Elements = " + str(len(newSequence)))
+        return True
+    else:
+        print("array = " + str(array) + " Elements = " + str(len(array)))
+        print("sequence = " + str(sequence) + " Elements = " + str(len(sequence)))
+        print("subsequence = " + str(newSequence) + " Elements = " + str(len(newSequence)))
+        return False
 
 array = [5, 1, 22, 25, 6, -1, 8, 10]
-sequence = [5, 1, 22, 22, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
 
 print(isValidSubsequence(array, sequence))
